ee7ec2eefa5edff5982b1e30eaae246b7c7efd8e
Luciano Santos <lucianobrsantos97@gmail.com>
2026-09-03 15:07:44 -0300

    Delete src directory

--- src/modelo/Revista.java
+++
@@ -1,4 +0,0 @@
-public class Revista extends ItemBiblioteca{
-
-
-}
--- src/modelo/Usuario.java
+++
@@ -1,8 +0,0 @@
-public abstract class Usuario{
-    private final String nome;
-    private int quantidadeEmprestada;
-
-    public abstract limiteItens();
-
-
-}
